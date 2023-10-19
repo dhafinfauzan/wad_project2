410ed6e46396ffca1fadd1c87f76b97a515c1131
dhafinfauzan <92373017+dhafinfauzan@users.noreply.github.com>
2023-10-19 23:42:16 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Selamat Datang/h1>
+
+<?php
+echo "Hello World!";
+$a = 50;
+$b = 10;
+if ($a > $b){
+    echo"gacor king";
+}
+?>
+
+</body>
+</html>
---
+++ latihan2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<form action="/action_page.php">
+  <label for="fname">Masukkan Angka:</label><br>
+  <input type="text" id="fname" name="fname" value="1,2,3"><br>
+  <select name="Angka" id="angka"><br>
+  <option value="1">1</option><br>
+  <option value="2">2</option>
+</select>
+<br></br>
+  <button type="button" onclick="alert('Gacor King')">Hitung</button>
+</form>
+</html>
